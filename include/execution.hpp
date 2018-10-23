eddfe2fc4647ca09627cf534467e384b0593214c
Jérémy Demeule <jeremy.demeule@gmail.com>
2018-10-23 16:50:28 +0200

    Add require Miss global customization point and variadic case.

--- include/execution.hpp
+++ include/execution.hpp
@@ -21,6 +21,7 @@
 
 #include <cstddef>
 #include <memory>
+#include <stdexcept>
 #include <type_traits>
 #include <typeinfo>
 
@@ -109,7 +110,7 @@ struct oneway_t;
 
 // Executor type traits:
 
-// template <class Executor, bool>
+// template <class Executor>
 // struct is_oneway_executor;
 // template <class Executor>
 // struct is_bulk_oneway_executor;
@@ -139,6 +140,7 @@ using executor = typename InterfaceProperty::template polymorphic_executor_type<
    InterfaceProperty,
    SupportableProperties...>;
 
+
 // template <class Property>
 // struct prefer_only;
 
@@ -147,6 +149,12 @@ using executor = typename InterfaceProperty::template polymorphic_executor_type<
 /////////////////////////////
 // Implementation
 /////////////////////////////
+
+class bad_executor : public std::runtime_error {
+public:
+   bad_executor()
+      : std::runtime_error("bad_executor") {}
+};
 
 // Type traits for C++11 compilers
 template <typename E, typename Fn>
@@ -194,19 +202,109 @@ struct is_oneway_executor : std::false_type {};
 template <class Executor>
 struct is_oneway_executor<Executor, true> : std::true_type {};
 
-struct require_fn {
-   template <class E, class P0>
-   constexpr auto operator()(E&& e, P0&&) const
-      -> std::enable_if_t<std::decay_t<P0>::is_requirable and
-                             (std::decay_t<P0>::template static_query_v<
-                                 std::decay_t<E>> == std::decay_t<P0>::value()),
-                          E> {
-      return std::forward<E>(e);
-   }
+
+// struct require_fn {
+//   template <class E, class P0>
+//   constexpr auto operator()(E&& e, P0&&) const -> std::enable_if_t<
+//      std::decay_t<P0>::is_requirable &&
+//         std::decay_t<P0>::template static_query_v<std::decay_t<E>>,
+//      std::decay_t<E>> {
+//      return std::forward<E>(e);
+//   }
+//   // The only supported query is on the main property (oneway_t /
+//   bulkoneway_t)
+//   // for now.
+//};
+
+// struct require_fn {
+//   template <
+//      class E,
+//      class P0,
+//      class E00 = std::decay_t<E>,
+//      class P00 = std::decay_t<P0>,
+//      class = std::enable_if<P00::is_requirable && P00::static_query_v<E00>>>
+//   constexpr std::decay_t<E> operator()(E&& e, P0&&) const {
+//      return std::forward<E>(e);
+//   }
+//};
+//
+// constexpr require_fn require = {};
+
+template <class E, class P, class = void>
+struct Require {
+   static const bool value = false;
 };
 
-constexpr require_fn require = {};
+template <class E, class P>
+struct Require<E, P, typename P::template static_query<E>> {
+   static const bool value = P::is_requirable && P::template static_query<E>::value;
+};
 
+template <class E>
+struct Require<E, oneway_t> {
+   static const bool value = is_oneway_executor<E>::value;
+};
+
+
+// Case N == 0, P0::is_requirable is true and P0::static_query_v<E> is well form
+// and valid
+template <class E,
+          class P0,
+          class E00 = std::decay_t<E>,
+          class P00 = std::decay_t<P0>>
+std::enable_if_t<Require<E00, P00>::value, E00> require(E&& e, P0&&) {
+   return std::forward<E>(e);
+}
+
+template <class T>
+struct has_require_method {
+   template <class C, class P>
+   static auto f(P* p) -> decltype(std::declval<C>().require(*p));
+
+   template <class, class>
+   static auto f(...) -> std::false_type;
+
+   struct anything {
+      template <class U>
+      operator U();
+   };
+
+   using type = decltype(f<T, anything>(nullptr));
+   static const bool value = !std::is_same<std::false_type, type>::value;
+};
+
+// template <class E,
+//          class P0,
+//          class E00 = std::decay_t<E>,
+//          class P00 = std::decay_t<P0>>
+// std::enable_if_t<!Require<E00, P00>::value && has_require_method<E00>::value,
+//                 typename has_require_method<E00>::type>
+// require(E&& e, P0&& p0) {
+//   return e.require(std::forward<P0>(p0));
+//}
+
+template <class T, class P>
+struct has_require_template_method {
+   template <class C>
+   static auto f(P* p) -> decltype(std::declval<C>().require(*p));
+
+   template <class>
+   static auto f(...) -> std::false_type;
+
+   using type = decltype(f<T>(nullptr));
+   static const bool value = !std::is_same<std::false_type, type>::value;
+};
+
+template <class E,
+          class P0,
+          class E00 = std::decay_t<E>,
+          class P00 = std::decay_t<P0>>
+std::enable_if_t<!Require<E00, P00>::value &&
+                    has_require_template_method<E00, P00>::value,
+                 typename has_require_template_method<E00, P00>::type>
+require(E&& e, P0&& p0) {
+   return e.require(std::forward<P0>(p0));
+}
 
 struct context_t {
    static constexpr bool is_requirable = false;
@@ -214,9 +312,10 @@ struct context_t {
 
    // using polymorphic_query_result_type = any;
 
-   template <class Executor>
-   static constexpr decltype(auto) static_query_v =
-      Executor::query(*static_cast<context_t*>(nullptr));
+   // No queries on it for now.
+   // template <class Executor>
+   // static constexpr decltype(auto) static_query_v =
+   //   Executor::query(*static_cast<context_t*>(nullptr));
 };
 
 struct oneway_t {
@@ -225,8 +324,13 @@ struct oneway_t {
 
    using polymorphic_query_result_type = bool;
 
+   // Not polymorphic on properties for now
    template <class... SupportableProperties>
    class polymorphic_executor_type;
+
+   // Extension to allow implementation in C++11 compiler
+   template <class Executor>
+   using static_query = is_oneway_executor<Executor>;
 
    template <class Executor>
    static constexpr bool static_query_v = is_oneway_executor<Executor>::value;
@@ -239,6 +343,27 @@ struct oneway_t {
 constexpr oneway_t oneway = {};
 
 
+template <class... Ts>
+struct type_list {};
+
+
+template <class P, class... Px>
+struct find_property;
+
+template <class P, class H, class... Tail>
+struct find_property<P, H, Tail...> : find_property<P, Tail...> {};
+
+template <class P, class... Tail>
+struct find_property<P, P, Tail...> : P {};
+
+template <class P>
+struct find_property<P> {};
+
+
+
+
+
+// Not polymorphic on properties for now
 template <class... SupportableProperties>
 class oneway_t::polymorphic_executor_type {
 public:
@@ -259,9 +384,10 @@ public:
       m_vptr->move_ctor(&m_buffer, &e.m_buffer);
    }
 
-   template <class... OtherSupportableProperties>
-   polymorphic_executor_type(
-      polymorphic_executor_type<OtherSupportableProperties...> e);
+   // Not Implemented for now.
+   // template <class... OtherSupportableProperties>
+   // polymorphic_executor_type(
+   //   polymorphic_executor_type<OtherSupportableProperties...> e);
 
    //   template <class... OtherSupportableProperties>
    //   polymorphic_executor_type(polymorphic_executor_type<OtherSupportableProperties...>
@@ -298,16 +424,25 @@ public:
 
    // polymorphic_executor_type operations:
 
-   template <class Property>
-   polymorphic_executor_type require(Property) const;
+   // Limitation: for now only the exact match instead of convertible one is
+   // handle.
+   template <class Property,
+             class Found = find_property<Property, SupportableProperties...>>
+   std::enable_if_t<Found::is_requirable, polymorphic_executor_type> require(
+      const Property& p) const {
+      if (!*this)
+         throw bad_executor();
+      return m_vptr->require(&m_buffer, typeid(Property), &p);
+   }
 
-   template <class Property>
-   typename Property::polymorphic_query_result_type query(Property) const;
+   // Not implemented for now.
+   // template <class Property>
+   // typename Property::polymorphic_query_result_type query(Property) const;
 
    // polymorphic_executor_type capacity:
 
    explicit operator bool() const noexcept {
-      return m_vptr != &empty;
+      return m_vptr != empty;
    }
 
    // polymorphic_executor_type target access:
@@ -332,9 +467,10 @@ public:
 
    // polymorphic_executor_type casts:
 
-   template <class... OtherSupportableProperties>
-   polymorphic_executor_type<OtherSupportableProperties...>
-   static_executor_cast() const;
+   // Not implemented for now.
+   // template <class... OtherSupportableProperties>
+   // polymorphic_executor_type<OtherSupportableProperties...>
+   // static_executor_cast() const;
 
 
 
@@ -366,7 +502,7 @@ public:
       if (!a && !b)
          return true;
       if ((a.m_vptr) == (b.m_vptr))
-         return true;
+         return a.m_vptr->eq(&a.m_buffer, &b.m_buffer);
       return false;
    }
 
@@ -401,7 +537,7 @@ private:
    // pointer or even lambda with capture.
    struct func_base {
       virtual ~func_base() = default;
-      virtual void call()  = 0;
+      virtual void call() = 0;
    };
 
    // Concrete form of `Function() -> void`.
@@ -420,68 +556,121 @@ private:
 
    using Buffer = typename std::aligned_storage<64>::type;
 
+
    struct vtable {
-      void (*dtor)(void* this_);
       void (*ctor)(void* this_);
       void (*copy)(void* this_, const void* rhs);
       void (*move_ctor)(void* this_, void* rhs);
-      const std::type_info& (*target_type)(const void* this_) noexcept;
+      void (*dtor)(void* this_);
+      bool (*eq)(const void* lhs, const void* rhs);
+      const std::type_info& (*target_type)(const void* this_);
       void (*execute)(const void* this_, std::unique_ptr<func_base> f);
+      polymorphic_executor_type (*require)(const void* this_,
+                                           const std::type_info& tp,
+                                           const void* p);
    };
+
+   template <class T>
+   struct vtable_model {
+      static void ctor(void* this_) {
+         new (this_) T();
+      }
+      static void copy_ctor(void* this_, const void* rhs) {
+         new (this_) T(*static_cast<const T*>(rhs));
+      }
+      static void move_ctor(void* this_, void* rhs) {
+         new (this_) T(std::move(*static_cast<T*>(rhs)));
+      }
+      static void dtor(void* this_) {
+         static_cast<T*>(this_)->~T();
+      }
+      static bool eq(const void* lhs, const void* rhs) {
+         return *static_cast<const T*>(lhs) == *static_cast<const T*>(rhs);
+      }
+      static const std::type_info& target_type(const void*) {
+         return typeid(T);
+      }
+      static void execute(const void* this_, std::unique_ptr<func_base> f) {
+         static_cast<const T*>(this_)->execute(
+            [f = std::move(f)]() mutable { f->call(); });
+      }
+      static polymorphic_executor_type require(const void* this_,
+                                               const std::type_info& tp,
+                                               const void* p) {
+         return require_(
+            static_cast<const T*>(this_), tp, p,
+            type_list<SupportableProperties...>{});
+      }
+
+      template <class Head, class... Tail>
+      static polymorphic_executor_type require_(const T* this_,
+                                                const std::type_info& tp,
+                                                const void* p,
+                                                type_list<Head, Tail...>) {
+         if (typeid(Head) == tp)
+            return execution::require(*this_, *static_cast<const Head*>(p));
+         return require_(this_, tp, p, type_list<Tail...>{});
+      }
+
+      static polymorphic_executor_type require_(const T*, const std::type_info&,
+                                                const void*, type_list<>) {
+         return {};
+      }
+   };
+
    template <class T>
    static const vtable* vtable_for() {
-      // clang-format off
       static vtable vptr = {
-         [](void* this_) {
-            static_cast<T*>(this_)->~T();
-         },
-         [](void* this_) {
-            new (this_) T();
-         },
-         [](void* this_, const void* rhs) {
-            new (this_) T(*static_cast<const T*>(rhs));
-         },
-         [](void* this_, void* rhs) {},
-         [](const void* this_) noexcept -> const std::type_info& {
-            return typeid(T);
-         },
-         [](const void* this_, std::unique_ptr<func_base> f) {
-            static_cast<const T*>(this_)->execute([f = std::move(f)]() mutable {
-               f->call();
-            });
-         }
-      };
-// clang-format on
-return &vptr;
-}  // namespace execution
+         &vtable_model<T>::ctor,      &vtable_model<T>::copy_ctor,
+         &vtable_model<T>::move_ctor, &vtable_model<T>::dtor,
+         &vtable_model<T>::eq,        &vtable_model<T>::target_type,
+         &vtable_model<T>::execute,   &vtable_model<T>::require };
+      return &vptr;
+   }
 
-static void                  default_dtor(void*) {}
-static void                  default_move_ctor(void*, void*) noexcept {}
-static const std::type_info& default_target_type(const void*) noexcept {
-   return typeid(void);
-}
+   struct default_vtable_model {
+      static void ctor(void*) {}
+      static void copy_ctor(void*, const void*) {}
+      static void move_ctor(void*, void*) {}
+      static void dtor(void*) {}
+      static bool eq(const void*, const void*) {
+         return true;
+      }
+      static const std::type_info& target_type(const void*) {
+         return typeid(void);
+      }
+      static void execute(const void*, std::unique_ptr<func_base>) {}
+      static polymorphic_executor_type require(const void*,
+                                               const std::type_info&,
+                                               const void*) {
+         return {};
+      }
+   };
 
-static const vtable empty;
 
-vtable const* m_vptr = &empty;
-Buffer        m_buffer;
-}
-;
+   static const vtable* default_vtable() {
+      static vtable vptr = {
+         &default_vtable_model::ctor,      &default_vtable_model::copy_ctor,
+         &default_vtable_model::move_ctor, &default_vtable_model::dtor,
+         &default_vtable_model::eq,        &default_vtable_model::target_type,
+         &default_vtable_model::execute,   &default_vtable_model::require };
+      return &vptr;
+   }
 
+
+   static const vtable* empty;
+
+   vtable const* m_vptr = empty;
+   Buffer m_buffer;
+};
+
+
+// Variant when will be polymorphic on properties.
 template <class... SupportableProperties>
 const typename oneway_t::polymorphic_executor_type<
-   SupportableProperties...>::vtable
-   oneway_t::polymorphic_executor_type<SupportableProperties...>::empty = {
-      &oneway_t::polymorphic_executor_type<
-         SupportableProperties...>::default_dtor,
-      nullptr,  // ctor
-      nullptr,  // copy
-      &oneway_t::polymorphic_executor_type<
-         SupportableProperties...>::default_move_ctor,
-      &oneway_t::polymorphic_executor_type<
-         SupportableProperties...>::default_target_type,
-      nullptr  // execute
-};
+   SupportableProperties...>::vtable*
+   oneway_t::polymorphic_executor_type<SupportableProperties...>::empty =
+      default_vtable();
 
 
 
--- test/ExecutionTest.cpp
+++ test/ExecutionTest.cpp
@@ -1,16 +1,104 @@
 #include "execution.hpp"
-#include "static_thread_pool.hpp"
+//#include "static_thread_pool.hpp"
+
+#include <memory>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 
+// struct vtable {
+//   void (*dtor)(void* this_);
+//   void (*ctor)(void* this_);
+//   void (*copy)(void* this_, const void* rhs);
+//   void (*move_ctor)(void* this_, void* rhs);
+//   const std::type_info& (*target_type)(const void* this_);
+//};
+//
+// template <class T>
+// struct vtable_model {
+//   static void ctor(void* this_) {
+//      new (this_) T();
+//   }
+//   static void copy_ctor(void* this_, const void* rhs) {
+//      new (this_) T(*static_cast<const T*>(rhs));
+//   }
+//   static void move_ctor(void* this_, void* rhs) {
+//      new (this_) T(std::move(*static_cast<T*>(rhs)));
+//   }
+//   static void dtor(void* this_) {
+//      static_cast<T*>(this_)->~T();
+//   }
+//   static const std::type_info& target_type(const void*) {
+//      return typeid(T);
+//   }
+//
+//   static const vtable vptr;
+//};
+//
+// template <class T>
+// const vtable vtable_model<T>::vptr = {&dtor, &ctor, &copy_ctor, &move_ctor,
+//                                      &target_type};
+//
+// template <>
+// struct vtable_model<void> {
+//   static void                  ctor(void*) {}
+//   static void                  copy_ctor(void*, const void*) {}
+//   static void                  move_ctor(void*, void*) {}
+//   static void                  dtor(void*) {}
+//   static const std::type_info& target_type(const void*) {
+//      return typeid(void);
+//   }
+//
+//   static const vtable vptr;
+//};
+//
+// template <>
+// const vtable vtable_model<void>::vptr = {&dtor, &ctor, &copy_ctor,
+// &move_ctor,
+//                                         &target_type};
+//
+// template <class T>
+// const vtable* vtable_for() {
+//   static vtable vptr = {&vtable_model<T>::dtor, &vtable_model<T>::ctor,
+//                         &vtable_model<T>::copy_ctor,
+//                         &vtable_model<T>::move_ctor,
+//                         &vtable_model<T>::target_type};
+//   return &vptr;
+//   // return &vtable_model<T>::vptr;
+//}
+//
+// struct inline_executor {
+//   friend bool operator==(const inline_executor&,
+//                          const inline_executor&) noexcept {
+//      return true;
+//   }
+//
+//   friend bool operator!=(const inline_executor&,
+//                          const inline_executor&) noexcept {
+//      return false;
+//   }
+//
+//   template <class Function>
+//   void execute(Function f) const noexcept {
+//      f();
+//   }
+//};
+//
+//
+// TEST(Foo, Bar) {
+//   const vtable* vtrp = vtable_for<inline_executor>();
+//}
+
+
 struct inline_executor {
-   friend bool operator==(const inline_executor&, const inline_executor&) noexcept {
+   friend bool operator==(const inline_executor&,
+                          const inline_executor&) noexcept {
       return true;
    }
 
-   friend bool operator!=(const inline_executor&, const inline_executor&) noexcept {
+   friend bool operator!=(const inline_executor&,
+                          const inline_executor&) noexcept {
       return false;
    }
 
@@ -21,11 +109,13 @@ struct inline_executor {
 };
 
 struct not_inline_executor {
-   friend bool operator==(const not_inline_executor&, const not_inline_executor&) noexcept {
+   friend bool operator==(const not_inline_executor&,
+                          const not_inline_executor&) noexcept {
       return true;
    }
 
-   friend bool operator!=(const not_inline_executor&, const not_inline_executor&) noexcept {
+   friend bool operator!=(const not_inline_executor&,
+                          const not_inline_executor&) noexcept {
       return false;
    }
 
@@ -46,6 +136,37 @@ struct thrower_executor {
    thrower_executor(const thrower_executor&) noexcept(false);
    template <class Function>
    void execute(Function f) const noexcept {}
+};
+
+struct custom_prop {
+   static constexpr bool is_requirable = true;
+   static constexpr bool is_preferable = true;
+   using polymorphic_executor_type = bool;
+
+   bool value = false;
+};
+
+struct custom_executor {
+   friend bool operator==(const custom_executor&,
+                          const custom_executor&) noexcept {
+      return true;
+   }
+
+   friend bool operator!=(const custom_executor&,
+                          const custom_executor&) noexcept {
+      return false;
+   }
+
+   custom_executor require(custom_prop p) const {
+      return {p.value};
+   }
+
+   template <class Function>
+   void execute(Function f) const noexcept {
+      f();
+   }
+
+   bool prop = false;
 };
 
 // template <class T>
@@ -70,17 +191,27 @@ TEST(Execution, 0) {
       void operator()() {}
    };
 
-//   static_assert(execution::is_executable_impl<inline_executor, nullary_fct>::value, "");
-//   static_assert(execution::is_executable_impl<inline_executor, nullary_fct&>::value, "");
-//   static const bool is_executable_cr = is_executable_impl<E, const nullary_fct&>::value;
-//   static const bool is_executable_rr = is_executable_impl<E, nullary_fct&&>::value;
-//
+   // static_assert(
+   //   execution::is_executable_impl<inline_executor, nullary_fct>::value, "");
+   // static_assert(
+   //   execution::is_executable_impl<inline_executor, nullary_fct&>::value,
+   //   "");
+   // static const bool is_executable_cr =
+   //   is_executable_impl<E, const nullary_fct&>::value;
+   // static const bool is_executable_rr =
+   //   is_executable_impl<E, nullary_fct&&>::value;
+
    static_assert(execution::is_oneway_executor<inline_executor>::value, "");
-   static_assert(!execution::is_oneway_executor<other_kind_of_executor>::value, "");
-   //   static_assert(!execution::is_oneway_executor<thrower_executor>::value, "");
+   static_assert(!execution::is_oneway_executor<other_kind_of_executor>::value,
+                 "");
+   // static_assert(!execution::is_oneway_executor<thrower_executor>::value,
+   // "");
 
    static_assert(
-      std::is_same<inline_executor, decltype(execution::require(inline_executor{}, execution::oneway_t{}))>::value, "");
+      std::is_same<inline_executor,
+                   decltype(execution::require(inline_executor{},
+                                               execution::oneway_t{}))>::value,
+      "");
 
    using executor = execution::executor<execution::oneway_t>;
 
@@ -147,9 +278,41 @@ TEST(Execution, 0) {
    }
 
    {
-      static_assert(execution::is_oneway_executor<inline_executor>::value, "one way executor requirements not met");
+      static_assert(execution::is_oneway_executor<inline_executor>::value,
+                    "one way executor requirements not met");
+      static_assert(execution::oneway_t::static_query<inline_executor>::value,
+                    "");
       inline_executor ex;
-      auto            ex2 = execution::require(ex, execution::oneway);
+      auto ex2 = execution::require(ex, execution::oneway);
+      ex2.execute([] { std::cout << "hello\n"; });
+   }
+
+   {
+      static_assert(
+         !execution::is_oneway_executor<other_kind_of_executor>::value, "");
+      static_assert(
+         !execution::oneway_t::static_query<other_kind_of_executor>::value, "");
+      // auto nop =
+      //   execution::require(other_kind_of_executor{}, execution::oneway);
+      // nop.execute([] {});
+   }
+
+   {
+      static_assert(execution::is_oneway_executor<custom_executor>::value, "");
+      static_assert(execution::has_require_method<custom_executor>::value, "");
+      auto ex2 = execution::require(custom_executor(), custom_prop{true});
+      EXPECT_TRUE(ex2.prop);
+   }
+
+   {
+      using EX = execution::executor<execution::oneway_t, custom_prop>;
+      EX ex5 = custom_executor{};
+      static_assert(
+         execution::has_require_template_method<EX, custom_prop>::value, "");
+      // auto ex6 = ex5.require(custom_prop{ true });
+      auto ex6 = execution::require(ex5, custom_prop{true});
+      const custom_executor* ex7 = ex6.target<custom_executor>();
+      EXPECT_TRUE(ex7->prop);
    }
 
    ex.execute([] { std::cout << "we made it\n"; });
